7a9eb4eda55df8b758b662dd6c0bd4fefbe460ad
Keri Deemer <kerid18@att.net>
2019-09-16 03:58:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,8 @@ def key_for_min_value(name_hash)
   min_value = 0 
   name_hash.each do | key, value |
     if min_value == 0 || value < min_value
+      min_value = value
+      min_key = key 
       
 
 end
